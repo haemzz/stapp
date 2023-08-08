908f373e19938307540d7fb92fed5fef5fe9e6cf
haemii <117583653+haemzz@users.noreply.github.com>
2023-08-09 06:45:05 +0900

    Update and rename iris.py to project2.py

--- project.py
+++ project.py
@@ -6,9 +6,9 @@ st.set_page_config('선호도테스트')
 # 질문지 체크
 tab1, tab2 = st.tabs(["고객 선호도 테스트", "선호도 기반 추천 공연"])
 with tab1:
-    _, col, _ = st.columns([2,6,2])
+    _, col, _ = st.columns([3,6,2])
     col.header('고객 선호도 테스트')
-    col.write('마음에 드는 대사를 골라보세요. (중복가능)')
+   
     user_genre = st.multiselect(
         '선호하는 공연 장르를 선택하세요.',
         ['오페라','뮤지컬','연극']
@@ -17,7 +17,7 @@ with tab1:
         st.write(':red[*장르를 선택해주세요!*]')
 
     user_list = []
-
+    st.write('#### 마음에 드는 대사를 골라보세요. (중복가능)')
     checkbox = st.checkbox('"카르페 디엠, 오늘을 즐겨라. 당신의 삶을 특별하게 만드세요."')
     if checkbox:
         user_list.extend(['Youth', 'Philosophical', 'Comedy'])
@@ -90,15 +90,74 @@ with tab1:
     if checkbox:
         user_list.extend(['Thriller', 'Realism', 'Horror']) 
 
-    checkbox = st.checkbox('대사가 없는 공연을 보고싶다.')
-    if checkbox:
-        user_list.extend(['Nonverbal']) 
+    # checkbox = st.checkbox('대사가 없는 공연을 보고싶다. (꼭)')
+    # if checkbox:
+    #     user_list.extend(['Nonverbal']) 
 
 with tab2:
-    df = pd.read_excel('공연_카테고리_라벨링.xlsx')
-    df_user = df[df.Genre.isin(user_genre)]
-    st.code(df.columns)
-    st.dataframe(df_user)
+    uploaded_file = st.file_uploader(
+        "Choose your database", accept_multiple_files=False)
+    if uploaded_file is not None:
+        file_name = uploaded_file
+    else:
+        file_name = "공연_카테고리_라벨링.xlsx"
+    df = pd.read_excel(file_name)
+    df = df[['Genre', 'Name', 'Category']]
+    # 전처리
+    df = df[df['Category'].notnull()].reset_index(drop=True)
+    df['Category'] = [a.split(',') for a in df['Category']]
+    df['Category'] = [sorted(a) for a in df['Category']]
+    # 수정할 것 수정하기
+    edit_dict = {'':None, 'Homorous':'Humorous','Circus':'Performance','kids':None,'Kids':None,'Tragidy':'Tragedy'}
+    for idx in df.index:
+        edit_list = []
+        for cat in df.at[idx,'Category']:
+            if cat in edit_dict.keys():
+                cat = edit_dict[cat]
+                edit_list.append(cat)
+            else:
+                edit_list.append(cat)
+        df.at[idx,'Category'] = edit_list
+    for idx in df.index:
+        if None in df.at[idx,'Category']:
+            df.at[idx,'Category'].remove(None)
+            
+    # 원본유지
+    shows_df = df.copy()
+    
+    user_name = 'User01'
+    user_record = [(user_genre, user_name, list(set(user_list)))]
+    user_df = pd.DataFrame(user_record, columns=df.columns.values)
+
+    # 유저가 선택한 장르에 해당하는 공연만, 그리고 유저 데이터를 추가하기
+    user_shows_df = shows_df[shows_df['Genre'].isin(user_genre)]
+    user_shows_df = user_shows_df.append(user_df).reset_index(drop = True)
+    
+    from sklearn.feature_extraction.text import CountVectorizer
+    user_shows_df['categories_literal'] = user_shows_df['Category'].apply(lambda x: (' ').join(x))
+    count_vect = CountVectorizer(min_df=0.00, ngram_range=(1,2))
+    category_mat = count_vect.fit_transform(user_shows_df['categories_literal'])
+    
+    from sklearn.metrics.pairwise import cosine_similarity
+    category_sim = cosine_similarity(category_mat,category_mat)
+    category_sim_sorted_ind = category_sim.argsort()[:,::-1]
+    
+    def find_sim_show(df,sorted_ind,title_name = user_name,top_n = 3):
+        # 기준이 되는 유저의 data와 index를 가져온다
+        title_show = df[df['Name'] == title_name]
+        title_index = title_show.index.values
+        
+        # (top_n)+1에 해당하는 장르 유서성이 높은 index 추출(유저 index를 삭제할 것이기 때문)
+        similar_indexes = sorted_ind[title_index,:(top_n)+1]
+        similar_indexes = similar_indexes.reshape(-1)
+        
+        # 유저 index는 제외
+        similar_indexes = similar_indexes[similar_indexes != title_index]
+        
+        return df.iloc[similar_indexes]
+    
+    similar_shows = find_sim_show(user_shows_df, category_sim_sorted_ind, user_name, 5)
+    st.dataframe(similar_shows)
 real_cats = [
     'Fantasy', 'Performance', 'History', 'Exciting', 'Nonverbal', 
     'Comedy', 'Crime', 'Serious', 'Mystery', 'Dark', 'Thriller', 
@@ -108,6 +167,6 @@ real_cats = [
     'Adventure', 'Horror', 'Science Fiction']
 
 st.code(set(user_list))
-st.code(len(set(user_list)))
+# st.code(len(set(user_list)))
 st.code(set(real_cats) - set(user_list))
 st.code(set(user_list) - set(real_cats))
